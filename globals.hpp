159ebbc93538bad2426a903278058f91a62fa7c8
Jamie Miles <147207363+Cherry-Trees@users.noreply.github.com>
2024-10-31 01:03:41 -0500

    Delete globals.hpp

--- globals.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBALS_HPP
-#define GLOBALS_HPP
-#include "State.hpp"
-
-std::unordered_map<std::string, std::vector<Rule>> non_terminals;
-std::unordered_set<State *> states;
-std::list<State *> states_in_order;
-
-#endif
