b4bb17c30684b1a3a046d98a2f0abe9090e4ce5f
Mallory Miles <147207363+Cherry-Trees@users.noreply.github.com>
2024-11-13 21:05:45 -0600

    Update State.hpp

--- src/State.hpp
+++ src/State.hpp
@@ -2,31 +2,29 @@
 #define STATE_HPP
 
 #include "Rule.hpp"
+#include "NonTerminal.hpp"
 #include <vector>
 #include <list>
 #include <map>
 #include <unordered_map>
 #include <unordered_set>
 
-class State;
+extern std::unordered_map<char, NonTerminal> u_nonterminal_set;
 
-extern std::unordered_map<std::string, std::vector<Rule>> non_terminals;
-extern std::unordered_set<State *> states;
-extern std::list<State *> states_in_order;
-
-class State
-{       
+class State {       
     public:
+        unsigned int index;
         std::list<Rule *> rules_in_order;
-        std::unordered_set<Rule, Rule::Hash> rules;
-        std::unordered_map<std::string, State *> adj;
+        std::unordered_set<Rule, Rule::Hash> rule_set;
+        std::unordered_map<char, State *> adj;
 
         Rule *top() const;
-        void add_rule(const Rule &rule);
+        bool add_rule(const Rule &rule);
         void add_rules(const std::vector<Rule> &rls);
         void take_closure(const Rule rule);
 
     friend std::ostream &operator<<(std::ostream &os, const State &state);
+    friend void debug_print(const State &state);
 };
 
 #endif
