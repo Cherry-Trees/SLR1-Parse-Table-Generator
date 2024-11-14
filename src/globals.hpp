9fbbb515072b7c16984e6c5cb92ba61a5d1a5b5e
Mallory Miles <147207363+Cherry-Trees@users.noreply.github.com>
2024-11-13 21:02:19 -0600

    Update globals.hpp

--- src/globals.hpp
+++ src/globals.hpp
@@ -1,9 +1,21 @@
 #ifndef GLOBALS_HPP
 #define GLOBALS_HPP
-#include "State.hpp"
 
-std::unordered_map<std::string, std::vector<Rule>> non_terminals;
-std::unordered_set<State *> states;
-std::list<State *> states_in_order;
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <set>
+#include "Rule.hpp"
+#include "State.hpp"
+#include "NonTerminal.hpp"
+
+std::unordered_set<char> u_terminal_set;
+std::unordered_map<char, NonTerminal> u_nonterminal_set;
+std::vector<Rule> rules_contain_nonterminal;
+std::set<State *> u_state_set;
+std::size_t state_index_counter = 0;
+
+std::vector<std::unordered_map<char, std::vector<std::string>>> action;
+std::vector<std::unordered_map<char, std::size_t>> go_to;
 
 #endif
