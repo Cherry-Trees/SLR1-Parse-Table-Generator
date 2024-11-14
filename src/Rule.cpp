41a1ac4b14831cf97bf189e1b09df9d3b01ab473
Mallory Miles <147207363+Cherry-Trees@users.noreply.github.com>
2024-11-13 21:03:31 -0600

    Update Rule.cpp

--- src/Rule.cpp
+++ src/Rule.cpp
@@ -1,21 +1,20 @@
 #include "Rule.hpp"
 #include <iostream>
 
-
 std::size_t Rule::Hash::operator()(const Rule &rule) const {
-    return std::hash<std::string>()(rule.rhs) ^ std::hash<unsigned int>()(rule.index);
+    return std::hash<char>()(rule.lhs) ^ std::hash<std::string>()(rule.rhs) ^ std::hash<unsigned int>()(rule.index);
+}
+bool Rule::Compare::operator()(const Rule &r1, const Rule &r2) const {
+    return Hash()(r1) < Hash()(r2);
 }
 
-Rule::Rule(const std::string lhs, const std::string rhs, const unsigned int index) : lhs(lhs), rhs(rhs), index(index) {}
 Rule::Rule() = default;
-Rule::Rule(const std::string lhs, const std::string rhs) : lhs(lhs), rhs(rhs), index(0) {}
 Rule::Rule(const Rule &other) : lhs(other.lhs), rhs(other.rhs), index(other.index) {}
-Rule::Rule(const std::string line) : lhs(line.substr(0, 1)), rhs(line.substr(3)), index(0) {}
-Rule::Rule (const char *line) : lhs{line[0]}, rhs(line + 3), index(0) {}
+Rule::Rule(const std::string line) : lhs(line[0]), rhs(line.substr(3)), index(0) {}
+Rule::Rule(const char lhs, const std::string rhs, const unsigned int index) : lhs(lhs), rhs(rhs), index(index) {}
 
 char &Rule::get() {return rhs[index];}
 char Rule::get() const {return rhs[index];}
-std::string Rule::get_str() const {return {rhs[index]};}
 
 Rule *Rule::operator&() const {return (Rule *)this;}
 Rule &Rule::operator=(const Rule &other) {
@@ -26,8 +25,11 @@ Rule &Rule::operator=(const Rule &other) {
 }
 
 Rule Rule::incremented() const {return Rule(lhs, rhs, index + 1);}
+bool Rule::eoi() const {return index == rhs.size();}
 bool Rule::operator==(const Rule &other) const {return rhs == other.rhs && index == other.index;}
 bool Rule::operator!=(const Rule &other) const {return rhs != other.rhs || index != other.index;}
 std::ostream &operator<<(std::ostream &os, const Rule rule) {
+    if (rule.rhs.empty())
+        return os << rule.lhs << "->" << "\u03B5_";
     return os << rule.lhs << "->" << rule.rhs.substr(0, rule.index) << '_' << rule.rhs.substr(rule.index);
 }
