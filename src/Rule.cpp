7a6c362775c07bd4298cd6d3eea665dc62400823
mallory <147207363+Cherry-Trees@users.noreply.github.com>
2024-12-16 20:14:52 -0600

    fixed lhs checking

--- src/Rule.cpp
+++ src/Rule.cpp
@@ -2,20 +2,17 @@
 #include <iostream>
 
 std::size_t Rule::Hash::operator()(const Rule &rule) const {
-    return std::hash<char>()(rule.lhs) ^ std::hash<std::string>()(rule.rhs) ^ std::hash<unsigned int>()(rule.index);
+    return std::hash<std::string>()(rule.lhs) ^ std::hash<std::string>()(rule.rhs) ^ std::hash<unsigned int>()(rule.index);
 }
-bool Rule::Compare::operator()(const Rule &r1, const Rule &r2) const {
-    return Hash()(r1) < Hash()(r2);
-}
-
+Rule::Rule(const std::string lhs, const std::string rhs, const unsigned int index) : lhs(lhs), rhs(rhs), index(index) {}
 Rule::Rule() = default;
+Rule::Rule(const std::string lhs, const std::string rhs) : lhs(lhs), rhs(rhs), index(0) {}
 Rule::Rule(const Rule &other) : lhs(other.lhs), rhs(other.rhs), index(other.index) {}
-Rule::Rule(const std::string line) : lhs(line[0]), rhs(line.substr(3)), index(0) {}
-Rule::Rule(const char lhs, const std::string rhs, const unsigned int index) : lhs(lhs), rhs(rhs), index(index) {}
-
+Rule::Rule(const std::string line) : lhs(line.substr(0, 1)), rhs(line.substr(3)), index(0) {}
+Rule::Rule (const char *line) : lhs{line[0]}, rhs(line + 3), index(0) {}
 char &Rule::get() {return rhs[index];}
 char Rule::get() const {return rhs[index];}
-
+std::string Rule::get_str() const {return {rhs[index]};}
 Rule *Rule::operator&() const {return (Rule *)this;}
 Rule &Rule::operator=(const Rule &other) {
     lhs = other.lhs;
@@ -23,11 +20,10 @@ Rule &Rule::operator=(const Rule &other) {
     index = other.index;
     return *this;
 }
-
 Rule Rule::incremented() const {return Rule(lhs, rhs, index + 1);}
 bool Rule::eoi() const {return index == rhs.size();}
-bool Rule::operator==(const Rule &other) const {return rhs == other.rhs && index == other.index;}
-bool Rule::operator!=(const Rule &other) const {return rhs != other.rhs || index != other.index;}
+bool Rule::operator==(const Rule &other) const {return lhs == other.lhs && rhs == other.rhs && index == other.index;}
+bool Rule::operator!=(const Rule &other) const {return lhs != other.lhs || rhs != other.rhs || index != other.index;}
 std::ostream &operator<<(std::ostream &os, const Rule rule) {
     if (rule.rhs.empty())
         return os << rule.lhs << "->" << "\u03B5_";
